4073a2ca73d50bd95834cfc83d41662b08dd0df1
zazbot5000 <zaskav@googlemail.com>
2019-05-22 23:51:38 +0100

    add drive_upload.py for uploding data to google drive

---
+++ modules/drive_upload.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from oauth2client import file, client, tools
+from googleapiclient.discovery import build
+from httplib2 import Http
+from apiclient.http import MediaFileUpload
+
+
+def g_authenticate():
+    # If modifying these scopes, delete the file token.json.
+    scopes = "https://www.googleapis.com/auth/drive"
+    store = file.Storage(Path("data/token.json"))
+
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(Path("data/credentials.json"), scopes)
+        creds = tools.run_flow(flow, store)
+    service = build("drive", "v3", http=creds.authorize(Http()))
+    return service
+
+
+def save_to_g_drive(service):
+    # Get the fileID from the drive sheet url
+    fileId = "1dYUGY54QYEnAfcd2JN_TUY1Sba3TRFAGvDQFM-WteKA"
+    _media = MediaFileUpload(
+        Path("output/export_dataframe.csv"), mimetype="text/csv", resumable=True
+    )
+    _updatedFile = service.files().update(fileId=fileId, media_body=_media).execute()
+
+    print("\nCSV data uploaded!")
+
+
+def main():
+    _service = g_authenticate()
+    save_to_g_drive(_service)
+    return
+
+
+if __name__ == "__main__":
+    main()
